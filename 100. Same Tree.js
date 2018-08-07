b67f5c6e10150f49c63ac10e8c2bc827f4522d53
cash <cash@cashde-MacBook-Pro.local>
2018-08-07 23:01:35 +0800

    [ADD]Same tree

---
+++ 100. Same Tree.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ * 
+ * 
+ * Given two binary trees, write a function to check if they are the same or not.
+ * Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+ * 題意:給定二個二元樹，檢查二顆查是否一樣(一樣的值以及一樣的結構)
+ * 
+ * 解題思路:檢查節點是否都有值或都沒值，檢查值、遞迴檢查左節點及右節點
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+
+
+
+var isSameTree = function(p, q) {
+ 
+    if ((p && !q) || (!p && q)) {
+        return false;
+    }
+    if (!p && !q) {
+        return true;
+    }
+
+    return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+};
