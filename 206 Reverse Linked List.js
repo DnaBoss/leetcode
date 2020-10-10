5ec486ddaccb570b0e30df2b3065cd119c86a686
ICO <math.exactly.one@gmail.com>
2020-10-10 16:01:32 +0800

    Create 206 Reverse Linked List.js

---
+++ 206 Reverse Linked List.js
@@ -0,0 +1,20 @@
+題目:Reverse a singly linked list.
+題意:反轉一個單向的鏈結串列
+思路:用三個變數記錄node，
+prev 記錄前一個
+current 記錄當前的
+next 記錄下一個
+不斷的把後一個的next 指向前一個node
+
+程式碼:
+var reverseList = function (head) {
+    let prev = null;
+    let current = head;
+    while (current) {
+        let next = current.next;
+        current.next = prev;
+        prev = current
+        current = next;
+    }
+    return current;
+};
