956edc7fa9c5cbdb12f0e9eeb82b4b12faa0612d
ICO <math.exactly.one@gmail.com>
2020-10-12 11:47:35 +0800

    Update 21.Merge Two Sorted Lists.js

--- 21.Merge Two Sorted Lists.js
+++ 21.Merge Two Sorted Lists.js
@@ -36,3 +36,21 @@ var mergeTwoLists = function(l1, l2) {
         current.next = l1;
     return root.next;
 };
+
+
+// 時隔四年，寫出 遞迴版本，之前到底為什麼跑不過我也忘了
+var mergeTwoLists = function (l1, l2) {
+    if (l1 == null) {
+        return l2;
+    }
+    if (l2 == null) {
+        return l1;
+    }
+    if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
+    }
+};
